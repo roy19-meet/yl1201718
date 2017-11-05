07c027fbc053a958358bc38f11c02ac1ad8dc0cf
idane19-meet <idane19@meet.mit.edu>
2017-11-05 18:28:44 +0200

    whatever

--- lab3/TurtleSapes.py
+++ lab3/TurtleSapes.py
@@ -1,31 +1,30 @@
+
 import turtle
-turtle.left(180)
-for i in range(5):
-	turtle.forward(100)
-	turtle.left(72)
-turtle.penup()
+#turtle.left(180)
+#for i in range(5):
+	#turtle.forward(100)
+	#turtle.right(144)
+#turtle.forward(100)
 
-turtle.goto(-100,0)
-turtle.left(180)	
-
-turtle.pendown()
-def triangles():
-	for i in range(3):
-		turtle.forward(100)
-		turtle.left(120)
-triangles()
-def star():	
-	turtle.left(192)
-	triangles()
-for i in range(5):
-	star()
+#turtle.goto(50,0)
+#turtle.goto(50,50)
+#turtle.left(120)
+#turtle.forward(50)
+#turtle.left(120)
+#turtle.forward(50)
+#turtle.goto(0,0)
+#turtle.register_shape("roy",((50,0),(50,50),(25,100),(0,50),(0,0)))
+#turtle.getshapes()
+#turtle.shape("roy")
 
 
-
-
+turtle.shape('apple.gif')
+print(turtle)
 
 
 
 
 turtle.mainloop()
+
+
 
